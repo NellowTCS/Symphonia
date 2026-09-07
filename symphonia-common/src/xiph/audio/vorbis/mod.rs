ee11e167bf3790241f391c0d74975300037c936f
NellowTCS <neeljaiswal23@gmail.com>
2026-09-07 15:16:50 -0600

    common:fmt

--- symphonia-common/src/xiph/audio/vorbis/mod.rs
+++ symphonia-common/src/xiph/audio/vorbis/mod.rs
@@ -115,8 +115,8 @@ pub fn unpack_xiph_laced_extradata(extradata: &[u8]) -> Result<(&[u8], &[u8])> {
 
 #[cfg(test)]
 mod tests {
-    use alloc::vec::Vec;
     use super::*;
+    use alloc::vec::Vec;
 
     #[test]
     fn test_unpack_xiph_laced_extradata_valid() {
